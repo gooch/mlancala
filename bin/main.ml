77a1315e5f1f984e8e249bc0ea5f53fc8c4687bc
Ross Paine <ashtermet@gmail.com>
2024-07-13 20:45:07 +0800

    main

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,6 @@ open Mancala
 
 let () =
   let board = init_board () in
-  (* Here you can add more logic to handle user input and game loop *)
   print_endline "Welcome to Mancala!\n";
   print_board board;
   game_loop board
